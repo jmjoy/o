0de4dda26e58fe2a817bae419f39fadde9f7fd4e
jmjoy <918734043@qq.com>
2015-08-10 07:16:09 +0800

    Modify hashset

--- hashset.go
+++ hashset.go
@@ -1,41 +1,34 @@
 package o
 
-type HashSet struct {
-	m    map[interface{}]struct{}
-	size int
+type HashSet map[interface{}]struct{}
+
+func NewHashSet() HashSet {
+	return NewHashSetCap(1)
 }
 
-func NewHashSet() *HashSet {
-	return NewHashSetCap(10)
+func NewHashSetCap(size int) HashSet {
+	return make(map[interface{}]struct{}, size)
 }
 
-func NewHashSetCap(size int) *HashSet {
-	hashset := new(HashSet)
-	hashset.m = make(map[interface{}]struct{}, size)
-	return hashset
+func (this HashSet) Add(i interface{}) {
+	this[i] = struct{}{}
 }
 
-func (this *HashSet) Add(i interface{}) {
-	this.m[i] = struct{}{}
-	this.size++
+func (this HashSet) Remove(i interface{}) {
+	delete(this, i)
 }
 
-func (this *HashSet) Remove(i interface{}) {
-	delete(this.m, i)
-	this.size--
-}
-
-func (this *HashSet) Has(i interface{}) bool {
-	_, ok := this.m[i]
+func (this HashSet) Has(i interface{}) bool {
+	_, ok := this[i]
 	return ok
 }
 
-func (this *HashSet) Size() int {
-	return this.size
+func (this HashSet) Size() int {
+	return len(this)
 }
 
-func (this *HashSet) Walk(f func(interface{}) error) error {
-	for i := range this.m {
+func (this HashSet) Walk(f func(interface{}) error) error {
+	for i := range this {
 		err := f(i)
 		if err != nil {
 			return err
